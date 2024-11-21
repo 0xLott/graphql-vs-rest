95bf15142bb696822d5226867358204eefc49c98
Yan <havercraft1@gmail.com>
2024-11-21 19:34:32 -0300

    Added response size

--- code/client/benchmark_script.py
+++ code/client/benchmark_script.py
@@ -2,13 +2,14 @@ import requests
 import time
 import csv
 import argparse
+import json
 
 parser = argparse.ArgumentParser(description='Benchmark REST vs GraphQL performance')
 parser.add_argument('--mode', choices=['java', 'javascript'], required=True, help="Language mode")
 args = parser.parse_args()
 
 if args.mode == 'java':
-    REST_BASE_URL = "http://localhost:8080/comments"
+    REST_BASE_URL = "http://localhost:8080/rest/comments"
     GRAPHQL_URL = "http://localhost:8080/graphql"
 elif args.mode == 'javascript':
     REST_BASE_URL = "http://localhost:3000/comments"
@@ -30,6 +31,13 @@ GRAPHQL_QUERIES = [
      "variables": {"score": 5}},
 ]
 
+graphql_responses = []
+rest_responses = []
+
+
+def count_chars_in_json_array(json_array):
+    combined_json_str = json.dumps(json_array)
+    return sum(1 for char in combined_json_str if not char.isspace())
 
 def benchmark_rest():
     results = []
@@ -38,6 +46,7 @@ def benchmark_rest():
             start_time = time.time()
             response = requests.get(endpoint["url"])
             end_time = time.time()
+            rest_responses.append(response.json())
             results.append({
                 "query": endpoint["name"],
                 "status_code": response.status_code,
@@ -55,6 +64,7 @@ def benchmark_graphql():
                 "query": query["query"],
                 "variables": query.get("variables", {}),
             })
+            graphql_responses.append(response.json())
             end_time = time.time()
             results.append({
                 "query": query["name"],
@@ -62,6 +72,7 @@ def benchmark_graphql():
                 "response_time_ms": (end_time - start_time) * 1000,
             })
     return results
+
 
 
 def save_to_csv(filename, data, fieldnames):
@@ -87,9 +98,30 @@ if __name__ == "__main__":
     save_to_csv(f"results/{args.mode}/response_time/rest_results.csv", rest_results, ["query", "status_code", "response_time_ms"])
     print("REST results saved to rest_results.csv")
 
+    save_to_csv(f"results/{args.mode}/response_size/rest_results.csv",
+    [
+            {
+                "protocol": "rest",
+                "total_response_size": count_chars_in_json_array(rest_responses)
+            }
+        ],
+        ["protocol", "total_response_size"]
+    )
+
     # Benchmark GraphQL
     print(f"Benchmarking GraphQL queries in {args.mode} mode...")
     graphql_results = benchmark_graphql()
     save_to_csv(f"results/{args.mode}/response_time/graphql_results.csv", graphql_results,
                 ["query", "status_code", "response_time_ms"])
+
+    save_to_csv(f"results/{args.mode}/response_size/graphql_results.csv",
+    [
+            {
+                "protocol": "graphql",
+                "total_response_size": count_chars_in_json_array(graphql_responses)
+            }
+        ],
+        ["protocol", "total_response_size"]
+    )
+
     print("GraphQL results saved to graphql_results.csv")
