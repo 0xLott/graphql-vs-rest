858afdc91e267a2a6285b100831f5014b5f31017
Letícia Lott <llott1710@gmail.com>
2024-11-21 00:06:37 -0300

    Added 'Get comments by id' route

--- code/javascript/api/rest.js
+++ code/javascript/api/rest.js
@@ -1,6 +1,7 @@
 const readJsonFile = require("../jsonLoader");
-
+const jp = require("jsonpath");
 const express = require("express");
+
 const app = express();
 const port = 3000;
 
@@ -9,7 +10,8 @@ filePath = "data/comments.json";
 async function loadData() {
   try {
     const data = await readJsonFile(filePath);
-    return data.comments;
+    const result = Object.assign({}, data.comments.row);
+    return result;
   } catch (err) {
     throw err;
   }
@@ -18,27 +20,32 @@ async function loadData() {
 app.get("/comments", async (req, res) => {
   try {
     const comments = await loadData();
-    res.send(comments);
+    return res.status(200).json({ success: true, data: comments });
   } catch (error) {
-    console.errorerror(error);
-    res.status(500).send("Error: Couldn't load comments.");
+    return res.status(500).json({ error: "Couldn't load comments." });
   }
 });
 
-app.get("/comments/:id", async (req, res) => {
-  commentId = req.params.id;
+app.get("/comment/:id", async (req, res) => {
+  const commentId = req.params.id;
 
-  try {
-    const data = await loadData();
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Error: Couldn't load comments.");
+  if (!commentId || isNaN(commentId)) {
+    return res.status(400).json({ error: "Invalid comment ID provided." });
   }
 
   try {
-    const comment = data.rows.Id[commentId];
+    const data = await loadData();
+
+    // JSONPath query
+    const comment = jp.query(data, `$[?(@.Id == ${commentId})]`);
+
+    if (!comment || comment.length === 0) {
+      return res.status(404).json({ error: `Comment with id ${commentId} not found.` });
+    }
+    return res.status(200).json({ success: true, data: comment });
   } catch (error) {
-    res.status(500).send(`Error: Couldn't find comment with id: ${commentId}`);
+    console.error("Error handling request:", error);
+    return res.status(500).json({ error: "Couldn't load comments." });
   }
 });
 
