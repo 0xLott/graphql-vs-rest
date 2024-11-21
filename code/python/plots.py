4ff2b3b999127ae81d5e3970cfd42188390fa658
Letícia Lott <llott1710@gmail.com>
2024-11-21 15:40:26 -0300

    Added plots and updated visualization script

--- code/python/plots.py
+++ code/python/plots.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import argparse
+import os
 
 parser = argparse.ArgumentParser(description='Benchmark REST vs GraphQL performance')
 parser.add_argument('--mode', choices=['java', 'javascript'], required=True, help="Language mode")
@@ -14,13 +15,13 @@ def load_results():
     return rest_results, graphql_results
 
 
-# Generate comparative plots
 def plot_comparative_performance():
-    # Load the results
     rest_results, graphql_results = load_results()
 
-    # Set up the seaborn style
     sns.set(style="whitegrid")
+
+    output_dir = f"results/plots/{args.mode}"
+    os.makedirs(output_dir, exist_ok=True)
 
     # Combine REST and GraphQL results into a single DataFrame for comparison
     rest_results['type'] = 'REST'
@@ -28,7 +29,7 @@ def plot_comparative_performance():
     combined_results = pd.concat([rest_results, graphql_results], ignore_index=True)
 
     # Bar plot of average response time for each query type
-    plt.figure(figsize=(12, 6))
+    plt.figure(figsize=(12, 12))
     avg_response_time = combined_results.groupby(['query', 'type'])['response_time_ms'].mean().unstack()
     avg_response_time.plot(kind='bar', stacked=False, width=0.8)
     plt.title(f'Average Response Time for {args.mode.capitalize()} - REST vs GraphQL')
@@ -36,25 +37,18 @@ def plot_comparative_performance():
     plt.ylabel('Average Response Time (ms)')
     plt.xticks(rotation=45, ha='right')
     plt.tight_layout()
+    plt.savefig(f"{output_dir}/average_response_time.png")
     plt.show()
 
     # Boxplot for response time distribution comparison
-    plt.figure(figsize=(12, 6))
-    sns.boxplot(x='query', y='response_time_ms', hue='type', data=combined_results)
+    plt.figure(figsize=(20, 10))
+    sns.boxplot(x='response_time_ms', y='query', hue='type', data=combined_results)
     plt.title(f'Response Time Distribution for {args.mode.capitalize()} - REST vs GraphQL')
     plt.xlabel('Query')
     plt.ylabel('Response Time (ms)')
     plt.xticks(rotation=45, ha='right')
     plt.tight_layout()
-    plt.show()
-
-    # Histogram of response times for both REST and GraphQL
-    plt.figure(figsize=(12, 6))
-    sns.histplot(data=combined_results, x='response_time_ms', hue='type', multiple="stack", bins=30)
-    plt.title(f'Response Time Distribution for {args.mode.capitalize()} - REST vs GraphQL')
-    plt.xlabel('Response Time (ms)')
-    plt.ylabel('Frequency')
-    plt.tight_layout()
+    plt.savefig(f"{output_dir}/response_time_distribution.png")
     plt.show()
 
 
